c1cc806cf01605c255c339cca4b2aaf1a50e3bbc
Ricardo Garcia <rigaro88@gmail.com>
2016-03-17 10:23:16 +1100

    Added Force class and generalized checkCollision

---
+++ Simulation/Force.m
@@ -0,0 +1,16 @@
+classdef Force
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        fx % Force magnitude in x direction
+        fy % Force magnitude in y direction
+        x % Force origin x location
+        y % Force origin y location
+    end
+    
+    methods
+    end
+    
+end
+
--- Simulation/checkCollision.m
+++ Simulation/checkCollision.m
@@ -1,9 +1,8 @@
-function [col, xCol, yCol] = checkCollision(finger1, finger2)
-    % Create finger polylines
-    fin1X = [finger1.origin(1) finger1.r_op(1) finger1.r_od(1)];
-    fin1Y = [finger1.origin(2) finger1.r_op(2) finger1.r_od(2)];
-    fin2X = [finger2.origin(1) finger2.r_op(1) finger2.r_od(1)];
-    fin2Y = [finger2.origin(2) finger2.r_op(2) finger2.r_od(2)];
-    [xCol, yCol] = polyxpoly(fin1X,fin1Y,fin2X,fin2Y);
+function [col, xCol, yCol] = checkCollision(object1, object2)
+    % Create object polylines
+    poly1 = object1.getPoly();
+    poly2 = object2.getPoly();
+    % Compute collision locations between the two objects
+    [xCol, yCol] = polyxpoly(poly1(1,:),poly1(2,:),poly2(1,:),poly2(2,:));
     col = ~(isempty(xCol) & isempty(yCol));
 end
